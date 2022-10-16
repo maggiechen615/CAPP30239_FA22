a30663428334ea65460d2f013939d44eaf89ec84
Maggie Chen <guochen615@foxmail.com>
2022-10-15 23:02:40 -0500

    changed axis font size

--- week_03/bar_chart_exercise/homework.js
+++ week_03/bar_chart_exercise/homework.js
@@ -3,9 +3,9 @@ d3.csv("library_visits_jan22.csv").then(data => {
         d.num = +d.num //convert num to a number
     };
 
-    const height = 400,
-    width = 800,
-    margin = ({top: 25, right: 30, bottom: 35, left: 50});
+    const height = 800,
+    width = 1200,
+    margin = ({top: 25, right: 30, bottom: 50, left: 5});
 
     //chart is a CSS selector string that acts as a container and contains joined SVG elements
     let svg =
@@ -24,16 +24,16 @@ d3.csv("library_visits_jan22.csv").then(data => {
     
     //build xAxis and yAxis through g to group SVG shapes
     const xAxis = g =>g 
-        .attr("transform", `translate(0, ${height - margin.bottom+5})`) //apply transform as attribute of g
+        .attr("transform", `translate(0, ${height - margin.bottom+10})`) //apply transform as attribute of g
         .call(d3.axisBottom(x));
     
      const yAxis = g =>g /*create a group*/
-        .attr("transform", `translate(${margin.left-5}, 0)`)
+        .attr("transform", `translate(${margin.left-10}, 0)`)
         .call(d3.axisLeft(y));
     
     //append a 'g' element to svg
-    svg.append("g").call(xAxis)
-    svg.append("g").call(yAxis) 
+    svg.append("g").style("font", "27px times").call(xAxis)
+    svg.append("g").style("font", "27px times").call(yAxis) 
 
 
     /*plot bar chart*/
@@ -44,7 +44,14 @@ d3.csv("library_visits_jan22.csv").then(data => {
         .attr("class", "bar");
     
     bar.append("rect")
-        .attr("fill", "steelblue")
+        //attempt to change bar color depending on value
+        .attr("fill", function(d) {
+            if (d.num < 1500) {
+                return 'lightgray';
+            } else if (d.num >= 1500) {
+                return 'steelblue'
+            }
+        })
         .attr("x", d => x(d.branch))
         .attr("width", x.bandwidth())
         .attr("y", d => y(d.num))
