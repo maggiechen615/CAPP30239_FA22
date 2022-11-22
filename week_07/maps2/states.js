a952ba7aad054d3f95da289c4d059896a3d044dd
Maggie Chen <guochen615@foxmail.com>
2022-11-22 17:15:28 -0600

    added week 7 content

---
+++ week_07/maps2/states.js
@@ -0,0 +1,24 @@
+const height = 610,
+  width = 975;
+
+const svg = d3.select("#chart")
+  .append("svg")
+  .attr("viewBox", [0, 0, width, height]);
+
+d3.json("libs/counties-albers-10m.json").then(us => {
+
+  const states = topojson.feature(us, us.objects.states); // Map simple geometries, could switch to states as well
+  console.log(us)
+
+  const path = d3.geoPath();
+
+  svg.append("g")
+    .selectAll("path")
+    .data(states.features)
+    .join("path")
+    .attr("stroke", "#999")
+    .attr("fill", "white")
+    // .attr("fill", d => (d.id in dataById) ? color(dataById[d.id].rate) : '#ccc')
+    .attr("d", path);
+
+});
